7485e34ff9266e8eed3580829eaef4f6c415051a
Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2022-10-27 22:20:26 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 9.99 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.py
@@ -0,0 +1,12 @@
+def solution(angle):
+
+    if angle < 90:
+        return 1
+    elif angle == 90:
+        return 2
+    elif angle > 90 and angle < 180:
+        return 3
+    else:
+        return 4
+    
+    return answer
