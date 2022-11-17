5079e00e307bc0fca6d666794c8ffc3d82e7923b
Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2022-11-17 22:23:53 +0900

    [Bronze V] Title: 나머지, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.py
@@ -0,0 +1,4 @@
+# 첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력
+
+a, b, c = map(int, input().split())
+print((a+b)%c, ((a%c)+(b%c))%c, (a*b)%c, ((a%c)*(b%c))%c, sep='\n')
