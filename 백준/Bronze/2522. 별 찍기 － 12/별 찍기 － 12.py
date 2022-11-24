0e0a29990fbe075afd182849082fd21bc0747dba
Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2022-11-25 07:03:43 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+for i in range(N):
+    for j in range(N - i - 1):
+        print(' ', end = "")
+    for j in range(i + 1):
+        print('*', end = "")
+    print()
+
+for i in range(N):
+    for j in range(i + 1):
+        print(' ', end = "")
+    for j in range(N - i - 1):
+        print('*', end = "")
+    print()
