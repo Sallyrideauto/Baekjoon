224585fe86a9e3d3f0029285f25b1f23c57b0d8e
Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2022-11-25 08:36:01 +0900

    [Bronze II] Title: 별 찍기 - 21, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+for i in range(N):
+    if N == 1:
+        print("*")
+    else:
+        if N % 2 == 0:
+            print("* " * (N // 2))
+            print(" *" * (N // 2))
+        else:
+            print("* " * (N // 2 + 1))
+            print(" *" * (N // 2))
