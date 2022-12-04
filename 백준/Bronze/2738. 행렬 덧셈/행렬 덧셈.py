6745575c54507104a928010dce475cada47707e1
Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2022-12-05 00:33:29 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 92 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,18 @@
+# https://develop247.tistory.com/93
+
+A, B = [], []
+
+N, M = map(int, input().split())
+
+for row in range(N):
+    row = list(map(int, input().split()))
+    A.append(row)
+
+for row in range(N):
+    row = list(map(int, input().split()))
+    B.append(row)
+    
+for row in range(N):
+    for col in range(M):
+        print(A[row][col] + B[row][col], end = ' ')
+    print()
