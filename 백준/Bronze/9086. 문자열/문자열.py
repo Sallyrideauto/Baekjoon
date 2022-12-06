c9f4a6c4452ef1b77442049febb7f73ba84cfabf
Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2022-12-06 20:02:09 +0900

    [Bronze V] Title: 문자열, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.py
@@ -0,0 +1,7 @@
+# https://oort-cloud.tistory.com/entry/%EB%B0%B1%EC%A4%80-9086%EB%B2%88-%ED%8C%8C%EC%9D%B4%EC%8D%ACpython-%EB%AC%B8%EC%9E%90%EC%97%B4
+
+x = int(input())
+
+for i in range(x):
+    a = str(input())
+    print(a[0] + a[-1])
