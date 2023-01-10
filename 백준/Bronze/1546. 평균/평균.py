e4631bc497d380eb9ac30d6b870f8ad1c5c9063c
Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2023-01-10 20:19:56 +0900

    [Bronze I] Title: 평균, Time: 36 ms, Memory: 30748 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,11 +1,8 @@
-N = int(input())
-scores = [int(N) for N in input().split()]
-M = int(max(scores))
+N = input() # 과목의 수
+mylist = list(map(int, input().split())) # 점수 정보가 담긴 리스트
+mymax = max(mylist) # mylist의 최대값
+sum = sum(mylist) # mylist의 모든 데이터값 더하기
 
-result = []
-
-for i in scores:
-    score = (i / M) * 100
-    result.append(score)
-
-print(sum(result) / N)
+# 총합*100/최고점수/과목수 계산 결과 출력
+# 한 과목과 관련된 수식을 총합한 후 관련 수식으로 변환해 로직을 간단하게 할 수 있음
+print(sum * 100 / mymax / int(N))
