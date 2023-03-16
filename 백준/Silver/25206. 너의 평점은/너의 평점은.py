c543adf6bd3a8d58618b1f61441e12a0fb9b5c89
Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2023-03-16 12:41:05 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,18 @@
+grades = {"A+": 4.5, "A0": 4.0, "B+": 3.5, "B0": 3.0, "C+": 2.5, "C0": 2.0, "D+": 1.5, "D0": 1.0, "F": 0.0}
+
+total_credit = 0
+total_grade = 0
+
+for i in range(20):
+    name, credit, grade = input().split()
+    credit = float(credit)
+    if grade == "P":
+        continue
+    total_credit += credit
+    total_grade += credit * grades[grade]
+
+if total_credit == 0:
+    print(0.0)
+else:
+    gpa = total_grade / total_credit
+    print(round(gpa, 4))
