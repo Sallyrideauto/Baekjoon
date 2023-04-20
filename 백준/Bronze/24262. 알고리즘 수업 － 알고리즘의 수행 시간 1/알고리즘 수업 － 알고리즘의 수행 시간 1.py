5d959e34044a50f2bacc9d1f5dfa71ca40e6e296
Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2023-04-20 11:56:36 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.py
@@ -0,0 +1,4 @@
+# https://v-0w0-v.tistory.com/entry/Python-%EB%B0%B1%EC%A4%80-24262-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EC%88%98%EC%97%85-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98%EC%9D%98-%EC%88%98%ED%96%89-%EC%8B%9C%EA%B0%84-1
+
+input()
+print(1,0,sep='\n')
