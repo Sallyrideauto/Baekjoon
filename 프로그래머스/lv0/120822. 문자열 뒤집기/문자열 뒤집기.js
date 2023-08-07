8251b8c19650425c387c5948c9ff79769167a3a0
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2023-08-07 16:32:06 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,5 @@
+function solution(my_string) {
+    return my_string.split("").reverse().join("");
+}
+
+// https://www.freecodecamp.org/korean/news/how-to-reverse-a-string-in-javascript-in-3-different-ways/
