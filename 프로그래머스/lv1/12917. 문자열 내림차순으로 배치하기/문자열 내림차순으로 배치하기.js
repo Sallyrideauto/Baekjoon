3bdd1560390528bb0c77c3aa2a2c9c9344e5a356
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2023-08-14 17:56:47 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -0,0 +1,5 @@
+function solution(s) {
+    return s.split('').sort().reverse().join('');
+}
+
+// https://velog.io/@760kry/%EC%9E%90%EB%B0%94%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EB%AC%B8%EC%9E%90%EC%97%B4-%EB%82%B4%EB%A6%BC%EC%B0%A8%EC%88%9C%EC%9C%BC%EB%A1%9C-%EB%B0%B0%EC%B9%98%ED%95%98%EA%B8%B0-sck2g6ce2n
