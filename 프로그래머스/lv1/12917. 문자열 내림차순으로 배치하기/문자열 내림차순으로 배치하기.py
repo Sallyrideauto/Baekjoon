27fbe156fb2d58ab612882b658298b85ba93f68e
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2023-10-06 23:23:39 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.00 ms, Memory: 9.98 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
@@ -0,0 +1,4 @@
+# https://velog.io/@joygoround/test-%EB%AC%B8%EC%9E%90%EC%97%B4-%EB%82%B4%EB%A6%BC%EC%B0%A8%EC%88%9C%EC%9C%BC%EB%A1%9C-%EB%B0%B0%EC%B9%98%ED%95%98%EA%B8%B0-%ED%8C%8C%EC%9D%B4%EC%8D%AC
+
+def solution(s):
+    return (''.join(reversed(sorted(s))))
