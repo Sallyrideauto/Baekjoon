11af8707231c6e2f971d5c83cf4de81d28fe66d5
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2023-11-02 15:17:17 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,10 @@
+phrase = input()
+
+if phrase == "SONGDO":
+    print("HIGHSCHOOL")
+elif phrase == "CODE":
+    print("MASTER")
+elif phrase == "2023":
+    print("0611")
+elif phrase == "ALGORITHM":
+    print("CONTEST")
