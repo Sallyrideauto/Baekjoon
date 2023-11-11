992c9b6c7ceb35e2a7d05efebf034b86dbc552c1
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2023-11-11 23:23:32 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,12 @@
+S = input()
+
+if S == 'M':
+    print('MatKor')
+elif S == 'W':
+    print('WiCys')
+elif S == 'C':
+    print('CyKor')
+elif S == 'A':
+    print('AlKor')
+elif S == '$':
+    print('$clear')
