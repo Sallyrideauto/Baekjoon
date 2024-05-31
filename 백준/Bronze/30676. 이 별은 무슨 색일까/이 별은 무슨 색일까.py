6b78f7311236e2dc36b97a959fc0094deb563fea
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2024-05-31 15:40:10 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,16 @@
+gamma = int(input())
+
+if gamma >= 620 and gamma <= 780:
+    print("Red")
+elif gamma >= 590 and gamma < 620:
+    print("Orange")
+elif gamma >= 570 and gamma < 690:
+    print("Yellow")
+elif gamma >= 495 and gamma < 570:
+    print("Green")
+elif gamma >= 450 and gamma < 495:
+    print("Blue")
+elif gamma >= 425 and gamma < 450:
+    print("Indigo")
+elif gamma >= 380 and gamma < 425:
+    print("Violet")
